c42e5e188c9d8daca5606c5cf2bd7d059fce20e5
Fainner Ramirez <framirez@godoycordoba.com>
2024-05-28 23:57:02 -0500

    go

---
+++ Hello-World/index.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt" // fmt significa format
+
+func main() {
+	fmt.Println("Hola mundo");
+}
---
+++ condicionales/index.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	day := 5;
+	if day == 5 {
+		fmt.Println("El dia es el 5");
+	} else if day == 6 {
+		fmt.Println("El dia es el 6");
+	} else if day == 7 {
+		fmt.Println("El dia es el 7");
+    } else {
+		fmt.Println("Valor no aceptado");
+	}
+}
